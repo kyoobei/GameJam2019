3d07eeff6eadc4df3bd7f0eb35c862ddc5b22989
Marion Ramos <marionmarloramos@artistryware.com>
2019-09-28 14:33:47 +0800

    Added UI animation

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public Animator startButtonAnimator;
+    public Animator HeaderTitleAnimator;
+    public void StartButtonSlideOut()
+    {
+        startButtonAnimator.SetBool("isHidden", true);
+        HeaderTitleAnimator.SetBool("isHidden", true);
+    }
+
+    public void StartButtonSlideIn()
+    {
+        HeaderTitleAnimator.SetBool("isHidden", false);
+    }
+
+
+
+}
